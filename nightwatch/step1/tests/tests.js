3306b1888100651f1b47093933401d7d25b9032f
Martinez <31023666+time2fishman@users.noreply.github.com>
2018-03-13 14:34:56 -0600

    1st test

--- nightwatch/step1/tests/tests.js
+++ nightwatch/step1/tests/tests.js
@@ -1,8 +1,21 @@
 module.exports = {
-    beforeEach : browser => {
+    beforeEach: browser => {
         browser.url('http://localhost:3000')
     },
-    after : browser => {
+    after: browser => {
         browser.end()
+    },
+
+    '3+3=6': browser => {
+        browser.waitForElementVisible('button[name="3Button"]', 3000)
+        browser.expect.element('span[name="result"]').text.to.equal('0')
+        browser.click('button[name="3Button"]')
+        browser.expect.element('span[name="result"]').text.to.equal('3')
+        browser.click('button[name="addButton"]')
+        browser.expect.element('span[name="result"]').text.to.equal('0')
+        browser.click('button[name="3Button"]')
+        browser.expect.element('span[name="result"]').text.to.equal('3')
+        browser.click('button[name="equalsButton"]')
+        browser.expect.element('span[name="result"]').text.to.equal('6')
     }
 }
